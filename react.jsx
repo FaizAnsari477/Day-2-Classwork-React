bb83a75422c425025b1d43798a45c35ecd2439f7
FaizAnsari477 <faizansari477@gmail.com>
2024-04-28 16:33:04 +0530

    Adding Day 2 Classwork

---
+++ react.jsx
@@ -0,0 +1,33 @@
+const Title = () => {
+    return (
+        <h1>Learn Web Development</h1>
+    )
+}
+
+const Para1 = () => {
+    return (
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, maxime. Sunt saepe dignissimos, aperiam autem incidunt ipsum laboriosam quasi tenetur adipisci aliquid nihil, fugit facere quisquam alias impedit dolore eveniet.</p>
+    )
+}
+const Para2 = () => {
+    return (
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem mollitia dolorum cupiditate dolore tempora eligendi commodi, voluptas labore qui aperiam alias similique aut, at ullam expedita culpa officiis corporis ratione error laborum! Quae, saepe tempore. Enim mollitia maxime odio vitae libero dolores fuga corporis perferendis sint ut est ducimus nihil, delectus deleniti neque! Iste animi sit suscipit et harum expedita deserunt. Facilis molestiae sint aperiam? Quam eum ab nihil magnam?</p>
+    )
+}
+const Para3 = () => {
+    return (
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas totam ipsam expedita minus harum quae nisi vero, voluptatibus dolores dicta, libero accusamus eligendi. Quo molestias aut esse, aspernatur totam doloribus. Modi ut illo fuga, voluptatum laborum incidunt ad, doloribus nam doloremque quisquam vero veritatis eligendi expedita, inventore animi totam quod. Autem, aperiam est? Aspernatur, culpa iusto cum inventore harum fugit nostrum veniam, maiores eveniet deleniti eaque? Quidem illum numquam cupiditate quas atque esse architecto corrupti excepturi dolor ipsam? Earum a ab rerum! Ducimus excepturi ratione esse ab necessitatibus nobis placeat.</p>
+    )
+}
+
+const Day2ClassWork = () => {
+    return (
+        <div>
+            <Title/>
+            <Para1/>
+            <Para2/>
+            <Para3/>
+        </div>
+    )
+}
+ReactDOM.render(<Day2ClassWork/>, document.querySelector('#main'));
